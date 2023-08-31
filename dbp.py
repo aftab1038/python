ae1f008195081bd9985c016645979f8ff781c8fd
aftab1038 <aftabk1038@gmail.com>
2023-08-31 07:51:22 +0500

    Data base project

--- dbp.py
+++ dbp.py
@@ -19,7 +19,9 @@ c.execute("""
                 std_gender TEXT NOT NULL,
                 std_phone NUMBER NOT NULL,
                 std_email TEXT NOT NULL,
-                std_address TEXT NOT NULL  
+                std_address TEXT NOT NULL,  
+            FOREIGN KEY (std_class) REFERENCES class (class_id) 
+            FOREIGN KEY (std_section) REFERENCES section (section_id)
             )
 """)
 c.execute("""
@@ -31,7 +33,8 @@ c.execute("""
                 teach_phone NUMBER NOT NULL,
                 teach_gender TEXT NOT NULL,
                 teach_subject TEXT NOT NULL,
-                teach_address TEXT NOT NULL 
+                teach_address TEXT NOT NULL,
+            FOREIGN KEY (teach_subject) REFERENCES subject (subject_id)
             )
                 
 """)
@@ -70,7 +73,11 @@ c.execute("""
                 obt_marks   INTEGER NOT NULL,
                 t_marks INTEGER NOT NULL,
                 percentage INTEGER NOT NULL,
-                rank INTEGER NOT NULL
+                rank INTEGER NOT NULL,
+            FOREIGN KEY (std_id) REFERENCES student (std_id)
+            FOREIGN KEY (subject_id) REFERENCES subject (subject_id)
+            FOREIGN KEY (class_id) REFERENCES class (class_id)
+            FOREIGN KEY (section_id) REFERENCES section (section_id)
             )
           
 """)
@@ -82,7 +89,8 @@ c.execute("""
                 hw_assign_date TEXT NOT NULL,
                 hw_deadline TEXT NOT NULL,
                 hw_description TEXT NOT NULL,
-                hw_marks INTEGER NOT NULL
+                hw_marks INTEGER NOT NULL,
+            FOREIGN KEY (hw_subject) REFERENCES subject (subject_id)
             )
 """)
 c.execute("""
@@ -92,7 +100,8 @@ c.execute("""
                 vol_type TEXT NOT NULL,
                 vol_date_time TEXT NOT NULL,
                 vol_description TEXT NOT NULL,
-                vol_reported_by TEXT NOT NULL
+                vol_reported_by TEXT NOT NULL,
+            FOREIGN KEY (vol_reported_by) REFERENCES teacher (teach_id) 
             )
 """)
 c.execute("""
@@ -100,8 +109,9 @@ c.execute("""
           
             (   activity_id INTEGER PRIMARY KEY AUTOINCREMENT, 
                 activity_type TEXT NOT NULL,
-                head_by TEXT NOT NULL,
-                activity_description TEXT NOT NULL
+                activity_head_by TEXT NOT NULL,
+                activity_description TEXT NOT NULL,
+            FOREIGN KEY (activity_head_by) REFERENCES teacher (teach_id)
             )
 """)
 c.execute("""
@@ -110,7 +120,9 @@ c.execute("""
             (   club_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 club_name TEXT NOT NULL,
                 club_teacher TEXT NOT NULL,
-                club_head TEXT NOT NULL
+                club_head TEXT NOT NULL,
+            FOREIGN KEY (club_teacher) REFERENCES teacher (teach_id)
+            FOREIGN KEY (club_head) REFERENCES student (std_id)
             )
 """)
 conn.commit()
